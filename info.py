92ee55228a70793cb8943b96e3f120eb60915d32
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-02-01 10:39:10 +0530

    Bei

--- info.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import environ
-
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", None)
-SESSION = os.environ.get("SESSION")
-ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1914868167").split())
-TIME = os.environ.get("TIME", None)
-GROUPS = [int(admin) for admin in environ.get("GROUPS", "").split()]
